45db28415caeaa74115d96deb12963cacb8fa902
akshatvermavi <akshatvermanike@gmail.com>
2023-10-31 15:57:59 +0530

    Reversing a 3-digit number a file is now updated

---
+++ C/reverse3digitno.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, reversedNum = 0;
+
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &num);
+
+    // Check if the input is a 3-digit number
+    if (num >= 100 && num <= 999) {
+        // Reverse the number
+        reversedNum += (num % 10) * 100;
+        num /= 10;
+        reversedNum += (num % 10) * 10;
+        num /= 10;
+        reversedNum += num;
+        printf("Reversed number: %d\n", reversedNum);
+    } else {
+        printf("Please enter a 3-digit number.\n");
+    }
+
+    return 0;
+}
